1a69e8011855758f172f5c2bd08be602c7801970
jalvarado1381 <jalvarado1381@users.noreply.github.com>
2015-06-11 15:01:48 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -3,9 +3,9 @@ featuresVar<-read.table("features.txt")
 
 xtest<-read.table("test/X_test.txt")
 
-testSubjects<-read.table()
+testSubjects<-read.table("test/subject_test.txt")
 
-testActivities<-read.table()
+testActivities<-read.table("test/y_test.txt")
 
 xtest2<-cbind(testSubjects,cbind(testActivities, xtest))
 
@@ -13,9 +13,9 @@ names(xtest2) <- c("Subject", "Activities", featuresVar$V2)
 
 xtrain<-read.table("train/X_train.txt")
 
-trainSubjects<-read.table()
+trainSubjects<-read.table("train/subject_train.txt")
 
-trainActivities<-read.table()
+trainActivities<-read.table("/y_test.txt")
 
 xtrain2<-cbind(testSubjects,cbind(testActivities, xtest))
 
