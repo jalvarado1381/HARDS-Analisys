41627c91c66ae29520ac760c3c096f45d91e137e
jalvarado1381 <juan.alvarado.1381@gmail.com>
2015-06-18 22:46:07 -0400

    from write.csv to write.table

--- run_analysis.R
+++ run_analysis.R
@@ -12,16 +12,16 @@ activityLabels<-read.table("UCI HAR Dataset/activity_labels.txt", stringsAsFacto
 xtest<-read.table("UCI HAR Dataset/test/X_test.txt")
 testSubjects<-read.table("UCI HAR Dataset/test/subject_test.txt")
 testActivities<-read.table("UCI HAR Dataset/test/y_test.txt")
-xtest2<-cbind(testSubjects,cbind(testActivities, xtest))
-names(xtest2) <- c("Subject", "Activities", featuresVar$V2)
+xtest<-cbind(testSubjects,cbind(testActivities, xtest))
+names(xtest) <- c("Subject", "Activities", featuresVar$V2)
 
 xtrain<-read.table("UCI HAR Dataset/train/X_train.txt")
 trainSubjects<-read.table("UCI HAR Dataset/train/subject_train.txt")
 trainActivities<-read.table("UCI HAR Dataset/train/y_train.txt")
-xtrain2<-cbind(trainSubjects,cbind(trainActivities, xtrain))
-names(xtrain2) <- c("Subject", "Activities", featuresVar$V2)
+xtrain<-cbind(trainSubjects,cbind(trainActivities, xtrain))
+names(xtrain) <- c("Subject", "Activities", featuresVar$V2)
 
-hards <- rbind(xtrain2,xtest2[1:length(xtest2[,1]),])
+hards <- rbind(xtrain,xtest[1:length(xtest[,1]),])
 names(hards) <- c("Subject", "Activities", featuresVar$V2)
 
 ############################################################################################
@@ -29,10 +29,11 @@ names(hards) <- c("Subject", "Activities", featuresVar$V2)
 ############################################################################################
 
 cat("2.- Extracting the measurements on the mean and standard deviation for each measurement.\n")
+
 meanstdRows <-c(grep("mean()", names(hards), fixed=TRUE), grep("std()",names(hards), fixed=TRUE))
 hards<-hards[c(1,2,meanstdRows)]
 
-rm(meanstdRows,xtrain2,xtest2,featuresVar, trainActivities,
+rm(meanstdRows,featuresVar, trainActivities,
    trainSubjects, testActivities,testSubjects, xtrain, xtest) ## removing objects
 
 ##############################################################################
@@ -41,11 +42,11 @@ rm(meanstdRows,xtrain2,xtest2,featuresVar, trainActivities,
 
 activityLabels<-read.table("UCI HAR Dataset/activity_labels.txt", stringsAsFactors=FALSE)
 cat("3.- Translating numbers to descritive names, for activities.\n")
-for (l in activityLabels[[1]]){
-  hards$Activities[hards$Activities==l] <- activityLabels[l,2]
+for (activity in activityLabels[[1]]){
+  hards$Activities[hards$Activities==activity] <- activityLabels[activity,2]
 }
 
-rm(l) ## removing objects
+rm(activity) ## removing objects
 
 ##########################################################################
 # 4.- Appropriately labels the data set with descriptive variable names. #
@@ -93,4 +94,4 @@ finalhards<-cbind(idfields, temphards)
 
 rm(subjectId, activity, activityLabels, hardsListSubjects, temphards, idfields ) ## removing objects
 
-write.csv(finalhards, "finalhards.txt", row.names=FALSE)
+write.table(finalhards, "finalhards.txt", sep=",", row.names=FALSE)
